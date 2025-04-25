f8894a5f7d84a048d37daa3b27a3fc1d50d205f6
jamesnorthrup <james.northrup@gmail.com>
2025-04-25 10:08:01 -0400

    fix: Manually correct migration SQL for PostgreSQL

--- prisma/migrations/20250420134024_init/migration.sql
+++ prisma/migrations/20250420134024_init/migration.sql
@@ -1,21 +1,22 @@
 -- CreateTable
 CREATE TABLE "Actor" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "imdbUrl" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    CONSTRAINT "Actor_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Rating" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "score" INTEGER NOT NULL,
     "note" TEXT,
     "actorId" INTEGER NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    CONSTRAINT "Rating_actorId_fkey" FOREIGN KEY ("actorId") REFERENCES "Actor" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    CONSTRAINT "Rating_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -23,3 +24,6 @@ CREATE INDEX "Actor_name_idx" ON "Actor"("name");
 
 -- CreateIndex
 CREATE INDEX "Rating_actorId_idx" ON "Rating"("actorId");
+
+-- AddForeignKey
+ALTER TABLE "Rating" ADD CONSTRAINT "Rating_actorId_fkey" FOREIGN KEY ("actorId") REFERENCES "Actor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
